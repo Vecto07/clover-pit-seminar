47f0fc8fc567b01cc72db53aa348f8bec47a5a03
vecto_07 <lutz.elias@outlook.de>
2026-09-24 17:13:03 +0200

    Ui Testing

--- src/main/java/com/tom/HelloDsl.java
+++ src/main/java/com/tom/HelloDsl.java
@@ -1,6 +1,9 @@
 package com.tom;
 
 import static dev.tamboui.toolkit.Toolkit.*;
+
+import dev.tamboui.layout.Flex;
+import dev.tamboui.style.Color;
 import dev.tamboui.toolkit.app.ToolkitApp;
 import dev.tamboui.toolkit.element.Element;
 
@@ -9,15 +12,26 @@ public class HelloDsl extends ToolkitApp {
 
     @Override
     protected Element render() {
-        return panel("Hello",
-            text("Welcome to TamboUI DSL!").bold().cyan(),
-            spacer(),
-            text("Press 'q' to quit").dim()
-        ).rounded();
+        String text = "";
+        return row(
+                panel("Left").fill().borderColor(Color.BLACK),
+                panel("Game",
+                    text("Welcome to CLOVER PIT!").bold().cyan(),
+                    spacer(),
+                    text("[☠️][💯][☀️][💫]").bold().cyan().centered(),
+                    spacer(),
+                    text("Press Space to Spin").bold().cyan().centered(),
+                    spacer(),
+                    text("Press 'q' to quit").dim()
+                        ).fill().rounded().flex(Flex.CENTER),
+                panel("Right").fill().borderColor(Color.BLACK)
+
+        );
     }
 
     public static void main(String[] args) throws Exception {
         // App starten
+
         new HelloDsl().run();
     }
 }
