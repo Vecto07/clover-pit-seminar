3eb25b74cf675d9f0b804bda8f9d95e63fc951d7
vecto_07 <lutz.elias@outlook.de>
2026-09-24 17:47:37 +0200

    Ui Testing

---
+++ src/main/java/com/tom/UI.java
@@ -0,0 +1,55 @@
+package com.tom;
+
+import static dev.tamboui.toolkit.Toolkit.*;
+
+import dev.tamboui.layout.Flex;
+import dev.tamboui.style.Color;
+import dev.tamboui.toolkit.app.ToolkitApp;
+import dev.tamboui.toolkit.element.Element;
+import dev.tamboui.toolkit.event.EventResult;
+
+
+public class UI extends ToolkitApp {
+    private String statusText = "Press Space to Spin";
+    @Override
+    protected Element render() {
+        String text = "";
+        return row(
+                panel("Left").fill().borderColor(Color.BLACK),
+                panel("Game",
+                    panel(
+                        text("Welcome to CLOVER PIT!").bold().cyan().centered(),
+                        spacer(),
+                        text("[☠️][💯][☀️][💫]").bold().cyan().centered(),
+                        spacer()
+                    )
+                        .percent(33)
+                        .rounded()
+                        .flex(Flex.CENTER),
+                    panel(
+                        text(statusText).bold().cyan().centered(),
+                        spacer(),
+                        text("Press 'q' to quit").dim().centered()
+                    )
+                        .fill()
+                        .rounded()
+                        .flex(Flex.CENTER)
+                ).fill().rounded().vertical().onKeyEvent(event -> {
+                    if (event.isChar(' ')) {
+                        statusText = "Space was pressed!";
+                        return EventResult.HANDLED;
+                    }
+
+                    return EventResult.UNHANDLED;
+                }),
+                panel("Right").fill().borderColor(Color.BLACK)
+
+        );
+    }
+
+    public static void main(String[] args) throws Exception {
+        // App starten
+
+        new UI().run();
+    }
+}
