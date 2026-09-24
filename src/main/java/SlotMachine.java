43bcf9d56fe1a38cafe1c29ae832f23d2beebcd3
num809 <tvdran@pm.me>
2026-09-24 17:43:55 +0200

    SlotMachine hinzugefügt

--- src/main/java/SlotMachine.java
+++ src/main/java/SlotMachine.java
@@ -1,21 +1,26 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class SlotMachine {
     private HashMap<Double, String> emojiProbabilities;
+    private int coins;
 
     public SlotMachine() {
+        this.coins = 0;
         this.emojiProbabilities = new HashMap<>();
         this.emojiProbabilities.put(0.44, "💩");
         this.emojiProbabilities.put(0.74, "❤️");
         this.emojiProbabilities.put(0.89, "👽");
         this.emojiProbabilities.put(0.94, "💯");
-        this.emojiProbabilities.put(0.99, "☠️");
+        this.emojiProbabilities.put(1.00, "☠️");
     }
 
     public String spin() {
         double spinResult = Math.random();
-        for(double mapContent: emojiProbabilities.keySet()) {
+        ArrayList<Double> hashMapKeys = new ArrayList<>(emojiProbabilities.keySet());
+        Collections.sort(hashMapKeys);
+        for(double mapContent: hashMapKeys) {
             if(spinResult > mapContent) {
                 continue;
             }
@@ -30,11 +35,30 @@ public class SlotMachine {
         ArrayList<String> emojis = new ArrayList<>();
         String output = "";
         for(int i = 1; i <= amount; i++) {
-            emojis.add("[" + this.spin() + "]");
+            String tempString = this.spin();
+            emojis.add("[" + tempString + "]");
+            coins = coins + this.calculateCoins(tempString);
         }
         for(String content: emojis) {
             output = output + (content + " ");
         }
-        return output;
+        return output.trim();
+    }
+
+    public int getCoins() {
+        int tempCoins = this.coins;
+        this.coins = 0;
+        return tempCoins;
+    }
+
+    public int calculateCoins(String emoji) {
+        switch(emoji) {
+            case("💩"): return(15);
+            case("❤️"): return(25);
+            case("👽"): return(45);
+            case("💯"): return(100);
+            case("☠️"): return(5);
+        }
+        return(0);
     }
 }
